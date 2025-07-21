9fe6cb0a30997ae9d35c9f9ca92421fc61414e85
wojciechc1 <wojtekcieciura@gmail.com>
2025-07-21 20:56:07 +0200

    update project

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import ctypes
 import os
 
-dll_folder = os.path.abspath("./rtl-sdr-64bit-20180506")
+dll_folder = os.path.abspath("rtl-sdr-64bit-20180506t")
 dll_path = os.path.join(dll_folder, "librtlsdr.dll")
 
 os.environ['PATH'] = dll_folder + ';' + os.environ['PATH']
